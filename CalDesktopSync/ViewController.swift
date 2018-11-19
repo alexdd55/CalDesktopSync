09b4914149a46f44284695a6ed4f36d68c9c3125
Alex M <alex.marquardt@gmail.com>
2018-11-19 18:35:56 +0100

    Initial Commit

---
+++ CalDesktopSync/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CalDesktopSync
+//
+//  Created by Alexander Marquardt on 19.11.18.
+//  Copyright © 2018 Alexander Marquardt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
